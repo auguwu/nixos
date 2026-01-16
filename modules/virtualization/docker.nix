e1c8f2c65bde3fa4be3e3a97984252049ad9428e
Noel Towa <cutie@floofy.dev>
2026-01-15 23:21:51 -0800

    [modules] i forgot to add docker

--- modules/virtualization/docker.nix
+++ modules/virtualization/docker.nix
@@ -1,1 +1,17 @@
-{}
+{pkgs, ...}: {
+  virtualisation.docker = {
+    enable = true;
+    daemon.settings = {
+      ipv6 = true;
+      fixed-cidr-v6 = "fd00::/80";
+      experimental = true;
+    };
+  };
+
+  environment.systemPackages = [
+    (pkgs.docker.override {
+      composeSupport = true; # installs and enables Docker Compose v2
+      buildxSupport = true; # uses buildx for building images
+    })
+  ];
+}
